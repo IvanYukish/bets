355205100707a54bdcd88740c33df50486a0b403
skhortiuk <khortiukserhii@ukr.net>
2019-09-26 17:40:33 +0300

    Added tennis

--- app/scrapers/bazabet/base.py
+++ app/scrapers/bazabet/base.py
@@ -1,6 +1,8 @@
+from asyncio import gather, create_task
+from datetime import datetime
 from itertools import chain
 
-import aiohttp
+from aiohttp import ClientSession
 
 from app.scrapers.base import BaseScrapper
 
@@ -9,6 +11,10 @@ class BazabetBaseScrapper(BaseScrapper):
     url = (
         "https://bookmakersapi.bazabet.com.ua/sportsbook"
         "/rest/public/sportMatches?ln=en"
+    )
+    detail_url = (
+        "https://bookmakersapi.bazabet.com.ua/sportsbook/rest/public/match"
+        "/?ln=en&mId={}"
     )
     game_id = None
     tags = []
@@ -23,31 +29,59 @@ class BazabetBaseScrapper(BaseScrapper):
         return f"{self.url}&id={self.game_id}"
 
     @staticmethod
-    def _get_cleared_games(raw_data: dict) -> list:
-        return [game for game in chain(*raw_data.values()) if "t" in game]
+    def _get_games_id(raw_data: dict) -> list:
+        return [game["id"] for game in chain(*raw_data.values())]
 
-    def filter(self, data: dict) -> list:
-        if not self.tags:
-            print("[!] Warning: `tags` is empty")
-        games = self._get_cleared_games(data)
+    async def _parse_simple(
+            self, item_id: int, session: ClientSession) -> dict:
+        async with session.get(self.detail_url.format(item_id)) as resp:
+            return await resp.json()
+
+    async def _parse_all(self, data: dict, session: ClientSession) -> list:
+        tasks = []
+        for game_id in self._get_games_id(data):
+            tasks.append(self._parse_simple(game_id, session))
+
+        return await gather(*tasks)
+
+    def _parse_event(self, event: dict) -> list:
+        res = []
+        for events in event.values():
+            for event in events:
+                if "translatableId" in event:
+                    continue
+
+                ev = {
+                    "type": event["n"],
+                    "cof": event["v"]
+                }
+
+                if "sbv" in event:
+                    ev["sub_value"] = event["sbv"]
+                res.append(ev)
+        return res
+
+    async def _cleared_data(self, parsed_games: list):
         result = []
-        for game in games:
-            coffs = {}
-            for cof in chain(*game["t"].values()):
-                if not self.tags or cof["n"] in self.tags:
-                    coffs[cof["n"]] = cof["v"]
-            if coffs:
-                result.append(
-                    {
-                        "name": game["n"],
-                        "events": coffs,
-                    }
-                )
-
+        for game in parsed_games:
+            if "t" in game:
+                events = self._parse_event(game["t"])
+                if events:
+                    result.append(
+                        {
+                            "name": game["n"],
+                            "date": datetime.strptime(
+                                game["sd"],
+                                "%Y-%m-%dT%H:%M:%S.%f%z"
+                            ).timestamp(),
+                            "events": events
+                        }
+                    )
         return result
 
     async def parse(self) -> list:
-        async with aiohttp.client.ClientSession() as session:
+        async with ClientSession() as session:
             async with session.get(self._game_type_url) as resp:
-                data = self.filter(await resp.json())
-        return data
+                data = await self._parse_all(await resp.json(), session)
+                s = await self._cleared_data(data)
+        return s
