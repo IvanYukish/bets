2e4c2e2c3f670e7c2a3df0f6275c68f90245eec3
skhortiuk <khortiukserhii@ukr.net>
2019-09-30 20:18:20 +0300

    Clean-up project

--- app/settings.py
+++ app/settings.py
@@ -8,9 +8,6 @@ SECRET = "ABCDEFG!@#$%#"
 
 DEBUG = False
 
-SCHEDULE_URL = "http://asu.pnu.edu.ua/cgi-bin/timetable.cgi?n=700"
-AJAX_URL = "http://asu.pnu.edu.ua/cgi-bin/timetable.cgi?"
-
 allowed_scrappers = {
     "bazabet": {
         "soccer": BazabetSoccerScrapper,
@@ -24,10 +21,6 @@ allowed_scrappers = {
 CONNECTION_TIMEOUT = 1.5
 REQUEST_TIMEOUT = 1.5
 
-REDIS_HOST = 'redis'
-REDIS_PORT = 6379
-
-CACHE_PERIOD = 60 * 60 * 24
 CORS = {
     'Access-Control-Allow-Origin': '*',
 }
