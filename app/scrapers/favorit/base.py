392a6cf10bf47be10e0d9eefdf636316e3cd02ad
yukish <iwan.jukisch@gmail.com>
2019-10-14 18:15:16 +0300

    fucking space

--- app/scrapers/favorit/base.py
+++ app/scrapers/favorit/base.py
@@ -2,8 +2,6 @@ import json
 
 import requests
 
-from collections import namedtuple
-
 from app.scrapers.base import BaseScrapper
 from app.scrapers.favorit.settings import match_id, service_id, method, \
     jsonrpc, lang, type_of_category
@@ -11,28 +9,76 @@ from app.scrapers.favorit.settings import match_id, service_id, method, \
 
 class FavoritBaseScrapper(BaseScrapper):
     url = (
-        "https://www.favorit.com.ua/en/frontend_api/top/"
+        'https://www.favorit.com.ua/frontend_api2/'
     )
 
-    detail_url = (
-        "https://www.favorit.com.ua/en/frontend_api/top/{}"
-    )
     game_id = None
 
     async def parse(self) -> list:
-        with requests.get(self._game_type_url) as resp:
-            data = resp.json()['sports'][0]['events']
+        with requests.post(self.url,
+                           json=self._generate_params_data_url()) as resp:
+            data = resp.json()['result']
             s = self._cleared_data(data)
         return s
 
     @property
-    def _game_type_url(self):
+    def _game_type_url(self) -> dict:
         if not self.game_id:
             print(
-                f"[!] Warning: `game_id` don't set. 1 will be used as default"
+                '[!] Warning: `game_id` don\'t set. 1 will be used as default'
             )
             self.game_id = 1  # soccer id
-        return f"{self.url}{self.game_id}"
+
+        params = {"jsonrpc": "2.0", "method": "frontend/category/get",
+                  "params": {
+                      "by": {"service_id": 0, "lang": "en",
+                             "sport_id": {"$in": [self.game_id]},
+                             "tz_diff": 10800}}, "id": 227}
+
+        return params
+
+    def _generate_category_id(self) -> list:
+        resp = requests.post(self.url, json=self._game_type_url).json()
+
+        return [elem['category_id'] for elem in resp['result']]
+
+    def _generate_tournament_id(self) -> list:
+
+        params = {"jsonrpc": "2.0", "method": "frontend/event/get",
+                  "params": {"by": {"service_id": 0, "lang": "en",
+                                    "category_id": None},
+                             "count": ["tournament_id"]}, "id": 1099}
+        tour_lst = []
+        for i, category_id in enumerate(self._generate_category_id()):
+            params['params']['by']['category_id'] = category_id
+            resp = requests.post(self.url, json=params).json()
+            for j in range(len(resp['result'])):
+                tour_lst.append(resp['result'][j]['tournament_id'])
+        return tour_lst
+
+    def _generate_params_data_url(self) -> dict:
+
+        params = {"jsonrpc": "2.0", "method": "frontend/event/get", "params": {
+            "by": {"lang": "en", "service_id": 0,
+                   "tournament_id": {"$in": self._generate_tournament_id()},
+                   "head_markets": True}},
+                  "id": 8021}
+
+        return params
+
+    @staticmethod
+    def _generate_params(game) -> dict:
+
+        event_id = game["event_id"]
+        url_params = json.dumps({'jsonrpc': jsonrpc, 'method': method,
+                                 'params': {'by': {'lang': lang,
+                                                   'service_id': service_id,
+                                                   'event_id': event_id}},
+                                 'id': match_id})
+
+        url = url_params
+
+        return url
 
     def _cleared_data(self, parsed_games: list) -> list:
         res = []
@@ -42,8 +88,8 @@ class FavoritBaseScrapper(BaseScrapper):
         }
 
         for game in parsed_games:
-            url = self.generate_url(game)
-            match_detail = requests.post(url.base_url, url.url_params).json()
+            params = self._generate_params(game)
+            match_detail = requests.post(self.url, params).json()
             name = game['event_name']
 
             api['games'] = {
@@ -56,24 +102,7 @@ class FavoritBaseScrapper(BaseScrapper):
         return res
 
     @staticmethod
-    def generate_url(game) -> namedtuple:
-
-        event_id = game["participants"][0]["event_id"]
-        base_url = "https://www.favorit.com.ua/frontend_api2/"
-
-        url_params = json.dumps({'jsonrpc': jsonrpc, 'method': method,
-                                 'params': {'by': {'lang': lang,
-                                                   'service_id': service_id,
-                                                   'event_id': event_id}},
-                                 'id': match_id})
-
-        class_url = namedtuple('url', ['base_url', 'url_params'])
-        url = class_url(base_url, url_params)
-
-        return url
-
-    @staticmethod
-    def _parse_event(event: dict) -> list:
+    def _parse_event(event: dict) -> dict:
         event_list = event["result"]
         only_full_time = [event for event in event_list if
                           event["result_type_name"] == 'Full Time']
@@ -81,28 +110,12 @@ class FavoritBaseScrapper(BaseScrapper):
         cleared_data = [event for event in only_full_time if
                         event['market_name'] in type_of_category]
 
-        item_list = []
+        item_list = {}
         for item in cleared_data:
             for i in range(len(item['outcomes'])):
-                item_list.append({
-                    'type': item["outcomes"][i]["outcome_short_name"],
-                    'cof': item["outcomes"][i]["outcome_coef"]
-                })
+                item_list[
+                    item["outcomes"][i]
+                    ["outcome_short_name"].replace('(', '').replace(')', '')] \
+                    = item["outcomes"][i]["outcome_coef"]
 
         return item_list
-
-
-class StartingSoon(FavoritBaseScrapper):
-    url = (
-        "https://www.favorit.com.ua/en/frontend_api/starting_soon/"
-    )
-
-    async def parse(self) -> list:
-        with requests.get(self._game_type_url) as resp:
-            data = resp.json()["starting_soon"]
-            s = self._cleared_data(data)
-        return s
-
-    @property
-    def _game_type_url(self):
-        return f"{self.url}"
--- app/scrapers/favorit/kinf_of_sports.py
+++ app/scrapers/favorit/kinf_of_sports.py
@@ -1,5 +1,5 @@
 from .settings import kinds_of_sport
-from .base import FavoritBaseScrapper, StartingSoon
+from .base import FavoritBaseScrapper
 
 
 def class_factory() -> dict:
@@ -8,8 +8,5 @@ def class_factory() -> dict:
     class_table = {}
     for k, v in kinds_of_sport.items():
         class_table[k] = type(k, (FavoritBaseScrapper,), {'game_id': v})
-
-    class_table['starting_soon'] = type('starting_soon',
-                                        (StartingSoon,), {})
 
     return class_table
